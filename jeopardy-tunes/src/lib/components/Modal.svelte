421e2d1cbd6e14923942ba4ed315625536304d33
MatJorgensen <bossjorgensen@gmail.com>
2023-09-26 20:48:12 +0000

    Add basic audio logic

--- jeopardy-tunes/src/lib/components/Modal.svelte
+++ jeopardy-tunes/src/lib/components/Modal.svelte
@@ -9,12 +9,19 @@
 	let showAnswer = false;
 	let modalContent: HTMLElement;
 
+	// audio
+	let playing = false;
+	let progress = 0;
+	let audio: HTMLAudioElement | null;
+
 	modalStore.subscribe(({ isOpen: open, question: q, answer: a, points: p }) => {
 		isOpen = open;
 		question = q;
 		answer = a;
 		points = p;
 		showAnswer = false;
+
+		resetAudio();
 	});
 
 	$: if (isOpen && modalContent) {
@@ -23,9 +30,10 @@
 
 	function closeModal(event: MouseEvent) {
 		if (event.target !== modalContent) {
-			modalStore.update(store => {
+			modalStore.update((store) => {
 				return { ...store, isOpen: false };
 			});
+			resetAudio();
 		}
 	}
 
@@ -35,10 +43,44 @@
 		}
 	}
 
+	// audio
+	function playPauseAudio() {
+		if (playing) {
+			audio?.pause();
+		} else {
+			audio?.play();
+		}
+		playing = !playing;
+	}
+
+	function updateProgress() {
+		if (audio?.currentTime && audio?.duration) {
+			progress = (audio?.currentTime / audio?.duration) * 100;
+		}
+	}
+
+	function resetAudio() {
+        if (audio) {
+            audio.pause();
+            audio.currentTime = 0;
+            playing = false;
+            progress = 0;
+        }
+    }
+
 	onMount(() => {
+		// question reveal
 		window.addEventListener('keyup', handleKeyup);
+
+		// audio
+		audio = new Audio('./audio/example.mp3');
+		audio.addEventListener('timeupdate', updateProgress);
+
 		return () => {
 			window.removeEventListener('keyup', handleKeyup);
+
+			audio?.removeEventListener('timeupdate', updateProgress);
+			audio = null;
 		};
 	});
 </script>
@@ -57,6 +99,10 @@
 				<p>{answer}</p>
 			{/if}
 			<p>Points: {points}</p>
+			<button on:click={playPauseAudio}>{playing ? 'Pause' : 'Play'}</button>
+			<div class="progress-bar">
+				<div class="progress" style="width: {progress}%" />
+			</div>
 			<button
 				on:click={(e) => {
 					e.stopPropagation();
@@ -84,6 +130,17 @@
 		background: white;
 		padding: 20px;
 		border-radius: 8px;
-        outline: none;
+		outline: none;
+	}
+
+	.progress-bar {
+		width: 100%;
+		height: 10px;
+		background-color: #f3f3f3;
+	}
+
+	.progress {
+		height: 100%;
+		background-color: #4caf50;
 	}
 </style>
